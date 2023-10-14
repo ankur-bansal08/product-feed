f18cc45c573a92bfdfa793251d39cc76e6ff7b8a
Ankur Bansal <5690056+ankur-bansal08@users.noreply.github.com>
2023-10-14 09:43:48 +0200

    Basic Spring boot code structure added

---
+++ src/main/java/com/tradedoubler/productfeed/ProductFeedApplication.java
@@ -0,0 +1,13 @@
+package com.tradedoubler.productfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductFeedApplication.class, args);
+	}
+
+}
