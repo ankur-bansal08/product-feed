d9a0f9a0fe66ecb477d5ce4a2e80fec0ed5f4a7d
Ankur Bansal <5690056+ankur-bansal08@users.noreply.github.com>
2023-10-26 19:01:36 +0200

    Failed Junit fix added. maven dependencies refactored

--- src/test/java/com/tradedoubler/productfeed/repository/ProductRepositoryTest.java
+++ src/test/java/com/tradedoubler/productfeed/repository/ProductRepositoryTest.java
@@ -1,12 +1,10 @@
 package com.tradedoubler.productfeed.repository;
 
-import com.tradedoubler.productfeed.repository.ProductRepository;
 import com.tradedoubler.productfeed.repository.entity.Product;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
@@ -14,14 +12,14 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-@TestPropertySource(locations = "classpath:application-test.properties")
 @ExtendWith(SpringExtension.class)
 public class ProductRepositoryTest {
 
     @Autowired
     ProductRepository productRepository;
+
     @Test
-    public void testRepositorySaveAndFind(){
+    public void testRepositorySaveAndFind() {
         String brand = "test_brand";
         Product savedProduct = productRepository.save(Product.builder().brand(brand).build());
 
---
+++ src/test/resources/schema.sql
@@ -0,0 +1,3 @@
+CREATE SCHEMA IF NOT EXISTS `product_feed`;
+
+
